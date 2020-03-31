0b4bd211c0df2897f5f0288ea87806d2b83bec5f
Alexander Egger <alexander.egger@egger.software>
2020-03-31 17:57:32 +0200

    Stupid calculator Android implementation

---
+++ kotlin-mpp-android/app/src/main/java/com/example/kotlinmppandroid/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinmppandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        calculateButton.setOnClickListener {
+
+            val numberA = inputA.text.toString().toDoubleOrNull() ?: 0.0
+            val numberB = inputB.text.toString().toDoubleOrNull() ?: 0.0
+            val resultValue = numberA + numberB
+            result.text = "Result: ${resultValue}"
+
+        }
+    }
+}
